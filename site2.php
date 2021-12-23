fdc4e5e515d0f4adff99804564b384197c020184
alancipher <alansame86@gmail.com>
2021-12-23 11:06:01 -0500

    one site file in php

---
+++ site2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+   <?php
+     echo "<h1>   hello world </h1>";
+     echo "<hr>";
+     echo "<p> this is my site</p>";
+    ?>
+
+  </body>
+</html>
