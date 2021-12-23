72bcbed78caa239740aeff31aaa7e1108ec8d5bb
alancipher <alansame86@gmail.com>
2021-12-23 16:04:16 -0500

    arrays in php

---
+++ site11.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <!-- a simple game   -->
+
+
+
+  <?php
+
+   $friends  = array("Kevin", "Karen", "Oscar", "Jim" );
+    $friends[4] = "Legend";
+    echo $friends[4];
+    echo "<br>";
+    echo count($friends);
+   ?>
+  </body>
+</html>
