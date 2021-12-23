65c2707a5bbac9e18238fb81d08b1806f940c06a
alancipher <alansame86@gmail.com>
2021-12-23 11:20:31 -0500

    dealing with variables inside of html and php

---
+++ site3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <!--varaibles in php  -->
+   <?php
+     $characterName = "the number";
+     $characterAge = 25;
+     echo "There once was a man named $characterName <br>";
+     echo "He was $characterAge years old <br>";
+
+     $characterName = "Mike";
+     echo "He really like the name $characterName <br>";
+     echo "But didn't like being $characterAge <br>";
+    ?>
+
+  </body>
+</html>
