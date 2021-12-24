ebd528cc65cb66ad895a596fa1ebc8615fb8ac54
alancipher <alansame86@gmail.com>
2021-12-24 14:45:15 -0500

    include php files  in php

---
+++ site22.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+  <!-- inclusion in php same header and same footer -->
+<?php
+$title = "My first post on that one !";
+$author = "Mike";
+$wordCount = 400;
+ include "article-header.php";
+ include "that.php";
+ sayHi("Freelance");
+ ?>
+  </body>
+</html>
---
+++ that.php
@@ -0,0 +1,10 @@
+<?php
+$feetInMile = 5280;
+function sayHi($name)
+{
+  echo "<br>";
+  echo "hello $name <br>";
+}
+
+
+ ?>
