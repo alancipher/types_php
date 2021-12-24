c598977e7f3ef0baad399de4da91c3bca5c19eb2
alancipher <alansame86@gmail.com>
2021-12-24 13:56:20 -0500

    a simple while loop  in php

---
+++ site20.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <!-- building a calculator
+
+ <form  action="site19.php" method="post">
+   what your grade ?:
+
+<input type="text" name="grade"> <br>
+
+ <input type="submit" >
+</form>-->
+
+  <?php
+  $index = 1;
+while ($index <=5 ) {
+  echo  "$index <br> ";
+  $index++;
+}
+   ?>
+  </body>
+</html>
