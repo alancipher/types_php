435ca1b706fd7517cb3e3ea7aec827d44948877c
alancipher <alansame86@gmail.com>
2021-12-28 13:59:19 -0500

    dealing with owl carousel

--- simpleProject/index.js
+++ simpleProject/index.js
@@ -2,8 +2,26 @@ $(document).ready(function(){
 
     // banner owl caroussel 
 
-    $("#banner-area .owl-carousel").owlCarousel({
-        dots:true,
-        items:1
-    });
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+
+   
+
 });
+
+
+
